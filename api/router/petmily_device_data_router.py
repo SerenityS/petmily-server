be8d8cf5301096ebfb9fd8a6fc7ca852d0c3f60f
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-06-03 04:50:58 +0900

    Revert "refactor: move post_device_data to ws"
    This reverts commit 3b17e89afdc2b801b3488bc263bb9a35e77ca471.

--- api/router/petmily_device_data_router.py
+++ api/router/petmily_device_data_router.py
@@ -9,6 +9,30 @@ from sqlalchemy.future import select
 def get_petmily_deivce_data_router() -> APIRouter:
     router = APIRouter()
 
+    @router.post(
+        "/device_data",
+        name="petmily: Post Device Data",
+    )
+    async def post_device_data(
+        device_data: DeviceData = None,
+    ):
+        async with async_session_maker() as session:
+            q = select(DeviceDataDB).where(DeviceDataDB.chip_id == device_data.chip_id)
+            result = await session.execute(q)
+
+            if result.scalars().first() is None:
+                session.add(DeviceDataDB(**device_data.dict()))
+            else:
+                q = (
+                    update(DeviceDataDB)
+                    .where(DeviceDataDB.chip_id == device_data.chip_id)
+                    .values(device_data.dict())
+                )
+                await session.execute(q)
+            await session.commit()
+
+        return {"message": "Device Data Registered"}
+
     @router.get(
         "/device_data",
         name="petmily: Get Device Data",
