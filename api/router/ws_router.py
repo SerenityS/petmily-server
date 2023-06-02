0c6b988fb20880e33bdfe0e18a571b691d7dd03f
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-06-03 04:10:45 +0900

    refactor: make ConnectionManager singleton
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- api/router/ws_router.py
+++ api/router/ws_router.py
@@ -71,6 +71,18 @@ async def send_device_data(device_data: DeviceData):
         await session.commit()
 
 
+def singleton(class_):
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+
+    return get_instance
+
+
+@singleton
 class ConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
